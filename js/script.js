1875a3246aad2768d659bac638a6ef67e9174cec
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-08 16:37:05 +0200

    Box aggiunti in html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 console.log("Ciao Mondo");
 
-// CREO CICLO PER I BOXE
+// CREO CICLO PER I BOX
 
 const container = document.querySelector(".container");
 
 const max = 1000
+
+for(let i=0; i <= max; i++){
+
+
+  const box = document.createElement("div");
+  box.className = "box";
+}
 
